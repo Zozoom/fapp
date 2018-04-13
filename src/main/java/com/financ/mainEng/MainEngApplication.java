cecf44a11f31e1087b8e217758995173314d08a2
Zoltan Zsigmond Kiss <kiss.zoltan.pti@gmail.com>
2018-04-13 11:16:21 +0200

    Base Commit

---
+++ src/main/java/com/financ/mainEng/MainEngApplication.java
@@ -0,0 +1,12 @@
+package com.financ.mainEng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainEngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainEngApplication.class, args);
+	}
+}
