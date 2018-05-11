47e1829b6db04e86195f9b020b585daa91bbcdcc
Zoltan Zsigmond Kiss <kiss.zoltan.pti@gmail.com>
2018-05-11 21:44:15 +0200

    Updates: * Logger Update/Upgrade + LogMonitor #24 * Log into files separately by day and size

--- src/main/java/com/financEng/FinancApplication.java
+++ src/main/java/com/financEng/FinancApplication.java
@@ -1,5 +1,7 @@
 package com.financEng;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,16 +13,29 @@ import java.net.URISyntaxException;
 @SpringBootApplication
 public class FinancApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(FinancApplication.class);
+
+	private static String myUrl = "http://localhost:8080/";
+
 	public static void main(String[] args) {
 		SpringApplication.run(FinancApplication.class, args);
-		openBrowser("http://localhost:8080/");
+		log.info("||////////////////////////////////////////////////////////////////////////////////////////||");
+		log.info("||                           FAPP (Financial Application)                                 ||");
+		log.info("||                             Created by Zoltan Kiss                                     ||");
+		log.info("||                                 Copyright 2018                                         ||");
+		log.info("||////////////////////////////////////////////////////////////////////////////////////////||");
+		log.info(">> [FinancApplication.main] - Starting Application ... Please Stand by...");
+		openBrowser(myUrl);
 	}
 
 	private static void openBrowser (String url){
+		log.info(">> [openBrowser.main] - Automatically open the app in a browser...");
+
 		if(Desktop.isDesktopSupported()){
 			Desktop desktop = Desktop.getDesktop();
 			try {
 				desktop.browse(new URI(url));
+				log.info(">> [openBrowser.main] - Browser opened at: {}",myUrl);
 			} catch (IOException | URISyntaxException e) {
 				e.printStackTrace();
 			}
